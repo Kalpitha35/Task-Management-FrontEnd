46df6ce37b4eb49cd7bd1afe061b3a4fe91582ef
Kalpitha35 <kalpithacj25@gmail.com>
2024-11-16 23:32:44 +0530

    change2

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,4 +1,4 @@
-const SERVER_URL = "https://task-management-server-5b1w.onrender.com"
+const SERVER_URL = "http://localhost:4000"
 export default SERVER_URL
 
-// 
+// https://task-management-server-5b1w.onrender.com
